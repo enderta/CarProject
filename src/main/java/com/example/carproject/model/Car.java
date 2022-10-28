16e2772ff3762208e92b39f5d87de358361d81e0
enderta <endertanver@gmail.com>
2022-10-28 14:53:48 +0100

    car

--- src/main/java/com/example/carproject/model/Car.java
+++ src/main/java/com/example/carproject/model/Car.java
@@ -18,9 +18,9 @@ private long id;
 private String brand, model, color,registerNumber;
 private int year, price;
 
-@OneToOne(cascade = CascadeType.ALL)
-@JoinColumn(name = "owner_id", referencedColumnName = "ownerId")
-private Owners owner;
+@OneToOne(mappedBy = "car")
+private Owners owners;
+
 
 
 
